b666282e3ce6cd3823300d3cf920c09d4defd64d
Jay Cincotta <jay.cincotta@gmail.com>
2022-10-16 17:56:34 -0700

    Integrated Formik/Yup on Login page

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,44 +1,76 @@
 import React, { useState, useContext } from "react"
 import { useNavigate, useSearchParams } from "react-router-dom";
 import { AppContext } from "../AppContext"
+import { useFormik } from "formik";
+import * as Yup from 'yup';
 
 export default function Login() {
   const { login } = useContext(AppContext)
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("")
   const [errorMsg, setErrorMsg] = useState("")
   const [params] = useSearchParams()
   const navigate = useNavigate();
 
-  const clickHandler = (e) => {
-    // https://bobbyhadz.com/blog/react-prevent-form-submit
-    e.preventDefault();
+  const validationSchema = Yup.object().shape({
+    email: Yup.string().required("Email is required").email("Email is invalid"),
+    password: Yup.string()
+      .required("Password is required")
+      .min(6, "Password must be at least 6 characters")
+      .max(40, "Password must not exceed 40 characters"),
+  })
 
-    const url = params.get('returnUrl') ?? "/"
-    login(email, password)
-    .then(() => navigate(url))
-    .catch(e => {
-      setErrorMsg(e.statusCode + ": " + e.message)
-    })
-  }
+  const formik = useFormik({
+    initialValues: {
+      email: "",
+      password: "",
+    },
+    validationSchema,
+    validateOnChange: false,
+    validateOnBlur: false,
+    onSubmit: (data) => {
+      console.log("SUBMIT", data)
+      const url = params.get('returnUrl') ?? "/"
+      login(data.email, data.password)
+        .then(() => navigate(url))
+        .catch(e => {
+          setErrorMsg(e.statusCode + ": " + e.message)
+        })
+    },
+  });
+
+  const denullify = value => value ? value : ""
+  const ifError = (name, yes, no) => formik.errors[name] && formik.touched[name] ? denullify(yes) : denullify(no)
+
+  const Error = ({ name }) => <div className="errorMessage">
+    {formik.errors[name] && formik.touched[name] ? formik.errors[name] : ""}
+  </div>
 
   return (
     <div className="login">
       <h1>Login</h1>
-      <form onSubmit={clickHandler}>
-      {errorMsg && <div className="errorMessage">{errorMsg}</div>}
-      <fieldset>
-        <label>Email:</label>
-        <input autoFocus value={email} onChange={(e) => setEmail(e.target.value)} />
-        <label>Password:</label>
-        <input
-          type="password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
-      </fieldset>
-      {/* <button onClick={clickHandler}>Login</button> */}
-      <button type="submit">Login</button>
+      <form onSubmit={formik.handleSubmit}>
+        {errorMsg && <div className="errorMessage">{errorMsg}</div>}
+        <fieldset>
+          <label>Email:</label>
+          <div>
+            <input autoFocus name="email" type="text"
+              className={ifError("email", "errorMessage")}
+              onChange={formik.handleChange}
+              value={formik.values.email}
+            />
+            <Error name="email" />
+          </div>
+          <label>Password:</label>
+          <div>
+            <input name="password" type="password"
+              className={ifError("password", "errorMessage")}
+              onChange={formik.handleChange}
+              value={formik.values.password}
+            />
+            <Error name="password" />
+          </div>
+        </fieldset>
+        {/* <button onClick={clickHandler}>Login</button> */}
+        <button type="submit">Login</button>
       </form>
     </div>
   );
