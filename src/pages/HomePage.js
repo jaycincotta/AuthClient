c05b2a87964e79c89b46dec9a3d97c9936cbde7a
Jay Cincotta <jay.cincotta@gmail.com>
2022-10-20 17:13:00 -0700

    Fixed web.config rewrite to handle routes

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,11 +1,21 @@
 import React, { useContext } from "react"
+import { Navigate  } from "react-router-dom";
 import { AuthContext } from "../context/AuthContext"
 import { DataContext } from "../context/DataContext"
 
 export default function HomePage() {
   const { claims } = useContext(AuthContext)
   const { count, increment } = useContext(DataContext)
-  console.log("Render HomePage")
+
+  // HACK: web.config rewrite rules weren't passing subpaths, just rewriting to home page
+  // so, the rewrite rule was rewritten to pass the subpaths as an argument
+  // The code below, parse those arguments and redirects to that page.
+  if (window.location.search.startsWith('?path=')) {
+    const redirectPath = window.location.search.substring(6).replace("&", "?")
+    console.log("REDIRECT", redirectPath)
+    return <Navigate to={redirectPath} replace={true} />
+  }
+
   return (
     <div>
       <h1>Home</h1>
